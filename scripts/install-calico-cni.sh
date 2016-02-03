57fdb2e50231c06c69edca99e5364f27e3c32cb0
Todd Nine <tnine@apigee.com>
2016-02-03 15:38:43 -0700

    Fixes bug in script to use the DOCKER_MACHINE_NAME variable

--- scripts/install-calico-cni.sh
+++ scripts/install-calico-cni.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Create necessary directories.
-docker-machine ssh dev sudo mkdir -p /etc/cni/net.d
-docker-machine ssh dev sudo mkdir -p /opt/cni/bin
-docker-machine ssh dev sudo mkdir -p /opt/bin
-docker-machine ssh dev sudo mkdir -p /var/run/calico
-docker-machine ssh dev sudo mkdir -p /var/log/calico
+docker-machine ssh $DOCKER_MACHINE_NAME sudo mkdir -p /etc/cni/net.d
+docker-machine ssh $DOCKER_MACHINE_NAME sudo mkdir -p /opt/cni/bin
+docker-machine ssh $DOCKER_MACHINE_NAME sudo mkdir -p /opt/bin
+docker-machine ssh $DOCKER_MACHINE_NAME sudo mkdir -p /var/run/calico
+docker-machine ssh $DOCKER_MACHINE_NAME sudo mkdir -p /var/log/calico
 
 # Ensure the network config file exists.
-docker-machine scp 10-calico.conf dev:/home/docker
-docker-machine ssh dev sudo mv /home/docker/10-calico.conf /etc/cni/net.d/
+docker-machine scp 10-calico.conf $DOCKER_MACHINE_NAME:/home/docker
+docker-machine ssh $DOCKER_MACHINE_NAME sudo mv /home/docker/10-calico.conf /etc/cni/net.d/
 
 # Download the CNI plugin.
-docker-machine ssh dev sudo wget -O /opt/cni/bin/calico https://github.com/projectcalico/calico-cni/releases/download/v1.0.0/calico
-docker-machine ssh dev sudo chmod +x /opt/cni/bin/calico
+docker-machine ssh $DOCKER_MACHINE_NAME sudo wget -O /opt/cni/bin/calico https://github.com/projectcalico/calico-cni/releases/download/v1.0.0/calico
+docker-machine ssh $DOCKER_MACHINE_NAME sudo chmod +x /opt/cni/bin/calico
 
-# Ensure proper kernel modules installed. 
-docker-machine ssh dev sudo modprobe -a xt_set ip6_tables
+# Ensure proper kernel modules installed.
+docker-machine ssh $DOCKER_MACHINE_NAME sudo modprobe -a xt_set ip6_tables
